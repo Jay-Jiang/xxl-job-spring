03ba2c9b8f3fe38e121e8af219559a71657870d9
Jay-Jiang <1540491116@qq.com>
2020-01-15 16:16:55 +0800

    更新依赖

--- xxl-job-executor/src/main/java/com/xxl/job/executor/service/jobhandler/SampleXxlJob.java
+++ xxl-job-executor/src/main/java/com/xxl/job/executor/service/jobhandler/SampleXxlJob.java
@@ -18,10 +18,12 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * XxlJob开发示例（Bean模式）
- *
+ * <p>
  * 开发步骤：
- * 1、在Spring Bean实例中，开发Job方法，方式格式要求为 "public ReturnT<String> execute(String param)"
- * 2、为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
+ * 1、在Spring Bean 实例中，开发Job 方法，方式格式要求为 "public ReturnT<String> execute(String param)"
+ * 2、为Job 方法添加注解
+ * - "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"
+ * - 注解value 值对应的是调度中心新建任务的 JobHandler 属性的值。
  * 3、执行日志：需要通过 "XxlJobLogger.log" 打印执行日志；
  *
  * @author xuxueli 2019-12-11 21:52:51
@@ -29,7 +31,6 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class SampleXxlJob {
     private static Logger logger = LoggerFactory.getLogger(SampleXxlJob.class);
-
 
     /**
      * 1、简单任务示例（Bean模式）
@@ -105,13 +106,13 @@ public class SampleXxlJob {
         if (exitValue == 0) {
             return IJobHandler.SUCCESS;
         } else {
-            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "command exit value("+exitValue+") is failed");
+            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "command exit value(" + exitValue + ") is failed");
         }
     }
 
 
     /**
-     * 4、跨平台Http任务
+     * 4、跨平台Http 任务
      */
     @XxlJob("httpJobHandler")
     public ReturnT<String> httpJobHandler(String param) throws Exception {
@@ -184,10 +185,11 @@ public class SampleXxlJob {
         return ReturnT.SUCCESS;
     }
 
-    public void init(){
+    public void init() {
         logger.info("init");
     }
-    public void destroy(){
+
+    public void destroy() {
         logger.info("destory");
     }
 
